b665b578fe109f0f1805a002baa76205d8b04139
Yam <yamprado2010@hotmail.com>
2022-01-18 23:12:19 -0300

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,26 @@
 module.exports = {
-  content: [],
+  content: [
+    './src/*.tsx',
+    './src/**/**/*.tsx',
+    './src/styles/*.scss',
+    './src/styles/**/*.scss',
+  ],
+  theme: {
+    colors: {
+      bl: '#15191C',
+      n90: '#1D232A',
+      n80: '#2C343A',
+      gr: '#95999C',
+      n20: '#D4D4D4',
+      n10: '#E7E7E7',
+      sewit: '#F3F3F3',
+      wt: '#FFFFFF',
+      blma: '#0480DC',
+      blho: '#0063AC',
+      blpr: '#004B83',
+      gr: '#1AB060',
+      red: '#F83636',
+    },
+
+  }
 };
