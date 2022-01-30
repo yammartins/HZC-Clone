73043e7663192bd6848b8b286516bd4ffaf56be8
Yam <yamprado2010@hotmail.com>
2022-01-30 11:33:42 -0300

    types for Section-Name

--- src/layouts/SectionName/index.tsx
+++ src/layouts/SectionName/index.tsx
@@ -1,18 +1,43 @@
+import Button from '~/components/Button';
+
 import { ReactComponent as ArrowIcon } from '../../assets/icons/arrow.svg';
+import { ReactComponent as LeftIcon } from '../../assets/icons/left.svg';
 import { ReactComponent as NotificationIcon } from '../../assets/icons/notifications.svg';
 import Picture from '../../assets/profile.jpeg';
+import { SectionNameHandles } from './types';
 
-const SectionName: React.FC = () => (
+const SectionName: React.FC<SectionNameHandles> = ({
+  server,
+  servername,
+  button,
+  profile,
+  name,
+  hasNotifications,
+}) => (
   <header className="section-channel">
     <div className="channel-text">
-      <h2 className="font-bold font-sans text-h3 text-wt mr-auto">Início</h2>
+      {server === true ? (<h2 className="font-bold font-sans text-h3 text-wt mr-auto">{servername}</h2>) : (
+        <span className="font-bold font-sans text-h3 text-wt mr-auto gap-2">
+          <LeftIcon />
+          {' '}
+          Retornar para a página anterior.
+        </span>
+      )}
     </div>
+    {button === true && (
+      <a className="link" href="algumacoisa">
+        <Button />
+        {' '}
+        Criar um novo pico
+        {' '}
+      </a>
+    )}
     <div className="profile">
       <div className="profile-data">
         <div className="picture">
-          <img src={Picture} alt="foto do usuário" />
+          <img src={profile} alt="foto do usuário" />
         </div>
-        <span className="font-sans text-normal font-normal text-grey">Yampmar</span>
+        <span className="font-sans text-normal font-normal text-grey">{name}</span>
         <div className="menu">
           <ArrowIcon />
         </div>
---
+++ src/layouts/SectionName/types.ts
@@ -0,0 +1,8 @@
+export interface SectionNameHandles {
+  server?: boolean;
+  servername?: string;
+  button: boolean;
+  profile: string;
+  name: string;
+  hasNotifications: boolean;
+}
