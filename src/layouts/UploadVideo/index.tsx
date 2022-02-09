e99e7000d8b3f4b597d58d66aa36c675748a96a2
Yam <yamprado2010@hotmail.com>
2022-02-09 15:44:30 -0300

    toogle for modals

--- src/layouts/UploadVideo/index.tsx
+++ src/layouts/UploadVideo/index.tsx
@@ -7,21 +7,28 @@ import { UploadVideoHandles } from './types';
 const UploadVideo: React.FC<UploadVideoHandles> = ({
   onPublish,
   ...rest
-}) => (
-  <Modal
-    {...rest}
-    name="Adicionar novo vídeo"
-  >
-    <div className="body">
-      <div className="icon">
-        <CloudIcon className="text-wt self-center m-auto w-10 h-10" />
+}) => {
+  const toogle = () => {
+    onPublish(true);
+    rest.onShow(false);
+  };
+
+  return (
+    <Modal
+      {...rest}
+      name="Adicionar novo vídeo"
+    >
+      <div className="body">
+        <div className="icon">
+          <CloudIcon className="text-wt self-center m-auto w-10 h-10" />
+        </div>
+        <h4 className="text-h4 text-wt font-bold">Arraste e solte o vídeo para iniciar o upload</h4>
+        <span className="text-menu text-wt/80 mb-2">Seu vídeo será publicado na plataforma</span>
+        <Button icon="plus" label="Selecionar arquivos" submit size="hg" onClick={toogle} />
       </div>
-      <h4 className="text-h4 text-wt font-bold">Arraste e solte o vídeo para iniciar o upload</h4>
-      <span className="text-menu text-wt/80 mb-2">Seu vídeo será publicado na plataforma</span>
-      <Button icon="plus" label="Selecionar arquivos" submit size="hg" onClick={() => onPublish(true)} />
-    </div>
-    <small className="text-leg text-wt/90 mx-auto">Cuidado! Alguns formatos de vídeo podem não funcionar.</small>
-  </Modal>
-);
+      <small className="text-leg text-wt/90 mx-auto">Cuidado! Alguns formatos de vídeo podem não funcionar.</small>
+    </Modal>
+  );
+};
 
 export default UploadVideo;
