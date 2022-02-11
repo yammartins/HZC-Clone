77dc12e40d83021474ab4bb0f264491428c6b628
Yam <yamprado2010@hotmail.com>
2022-02-11 11:53:12 -0300

    api

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,36 @@
 import Button from '~/components/Button';
 import Input from '~/components/Input';
-
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+ 
 import Logo from '../../assets/logo.svg';
+import api from '../../services';
 
-const Login: React.FC = () => (
+const Login: React.FC = () => {
+ 
+  const navigate = useNavigate();
+  const [error, onError] = useState(false);
+  const [text, onText] = useState('');
+  const [password, onPassword] = useState('');
+
+  const submit = async (e) => {
+    e.preventDefault();
+
+    await api.post('/auth/local', {
+      identifier: 'ymprado2010@gmail.com',
+      password: 'Ziniti123!',
+    })
+    .then((res) => {
+      //asdasd
+      console.log(res.data);
+    })
+    .catch((err) => {
+      //asdasd
+      console.log(err.response);
+    })
+  };
+ 
+ return (
   <main className="h-screen w-screen flex items-center justify-center">
     <div className="login-wrapper">
       <div className="welcome">
@@ -12,14 +39,29 @@ const Login: React.FC = () => (
         <p>Utilize seus dados para acessar a sua conta</p>
       </div>
       <div className="form">
-        <form action="" onSubmit>
-          <Input type="text" className="login" label="E-mail" way="e-mail" />
-          <Input type="password" className="login" label="Senha" way="password" />
+        <form action="" onSubmit={submit}>
+          <Input 
+          type="text" 
+          className="login" 
+          label="E-mail"
+          way="e-mail"
+          value={text}
+          onChange={({ target }) => onText(target.value)}
+           />
+          <Input 
+          type="password" 
+          className="login" 
+          label="Senha" 
+          way="password"
+          value={password}
+          onChange={({ target }) => onPassword(target.value)}
+           />
         </form>
         <Button submit full size="hg" label="Entrar" />
       </div>
     </div>
   </main>
 );
+};
 
 export default Login;
